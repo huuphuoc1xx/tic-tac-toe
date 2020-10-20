4515ba839224d26f58565b2c2f8910719c99b2ac
phuocnh3 <phuocnh@alive.vn>
2020-10-20 16:18:09 +0700

    Update function component

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,29 +1,28 @@
 import Square from "./Square";
-import React from 'react';
+import React from "react";
 
-export default class Board extends React.Component {
-
-
-  renderSquare(i) {
-    return <Square
-      value={this.props.squares[i]} onClick={() => this.props.onClick(i)}
-      winCell={this.props.winCell.includes(i)} />;
-  }
-  render() {
-
+export default function Board(props) {
+  const renderSquare = (i) => {
     return (
-      <div>
-        {(() => {
-          let src = [];
-          for (let i = 0; i < 3; i++) {
-            let child = [];
-            for (let j = 0; j < 3; j++)
-              child.push(this.renderSquare(i * 3 + j))
-          src.push(<div className="board-row">{child}</div>);
-          }
-          return src;
-        })()}
-      </div>
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+        winCell={props.winCell.includes(i)}
+      />
     );
-  }
+  };
+
+  return (
+    <div>
+      {(() => {
+        let src = [];
+        for (let i = 0; i < 3; i++) {
+          let child = [];
+          for (let j = 0; j < 3; j++) child.push(renderSquare(i * 3 + j));
+          src.push(<div className="board-row">{child}</div>);
+        }
+        return src;
+      })()}
+    </div>
+  );
 }
--- src/component/Game.js
+++ src/component/Game.js
@@ -1,94 +1,96 @@
-import React from 'react'
-import { calculateWinner } from '../helper/hepler';
-import Board from './Board';
+import React, {  useState } from "react";
+import { calculateWinner } from "../helper/hepler";
+import Board from "./Board";
 
-export default class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{ squares: Array(9).fill(null), stepLocate: [] }],
-      xIsNext: true,
-      stepNum: 0,
-      winner: null,
-      winCell: [],
-      reverse: false,
-    };
-  }
+export default function Game() {
+  const [history, setHistory] = useState([
+    { squares: Array(9).fill(null), stepLocate: [] },
+  ]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNum, setStepNum] = useState(0);
+  const [winner, setWinner] = useState(null);
+  const [winCell, setWinCell] = useState([]);
+  const [reverse, setReverse] = useState(false);
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNum + 1);
-    const current = history[history.length - 1];
+  const handleClick = (i) => {
+    const h = history.slice(0, stepNum + 1);
+    const current = h[h.length - 1];
     const squares = current.squares.slice();
 
-    if (this.state.winner || squares[i])
-      return;
+    if (winner || squares[i]) return;
 
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    calculateWinner(squares, this.setWinner);
-    this.setState({
-      history: history.concat([
+    squares[i] = xIsNext ? "X" : "O";
+    calculateWinner(squares, (winCell,winner) => {
+      setWinner(winner);
+      setWinCell(winCell);
+    });
+    setHistory(
+      h.concat([
         {
           squares,
-          stepLocate: [(i - i % 3) / 3, i % 3]
-        }
-      ]),
-      stepNum: history.length,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-  setWinner = (winCell, winner) => { this.setState({ winCell, winner }) }
-
-  jumpTo(step) {
-    this.setState({
-      stepNum: step,
-      xIsNext: (step % 2) === 0,
-      winCell: [],
-      winner: null
-    });
-    const history = this.state.history.slice(0, step + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    console.log(squares)
-    calculateWinner(squares,this.setWinner);
-  }
-
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNum];
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-        `Go to move (${step.stepLocate[0]},${step.stepLocate[1]}) ` :
-        'Go to game start';
-      return (
-        <li key={move}>
-          <button
-            onClick={() => this.jumpTo(move)}
-            className={this.state.stepNum === move ? "back-btn bold-btn" : "back-btn"}>{desc}</button>
-        </li>
-      );
-    });
-
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares}
-            winCell={this.state.winCell}
-            onClick={i => this.handleClick(i)} />
-        </div>
-        <div className="game-info">
-          <div>{this.state.stepNum === 9 && !this.state.winner ?
-            `Draw` :
-            this.state.winner ?
-              `Winner:${this.state.winner}` :
-              `Next player: ${this.state.xIsNext ? 'X' : 'O'}`}</div>
-          <button onClick={() => { this.setState({ reverse: !this.state.reverse }) }}>Reverse step</button>
-          <ol
-            reversed={this.state.reverse}
-            className={this.state.reverse ? "reverse-list" : ""} >{moves}</ol>
-        </div>
-      </div>
+          stepLocate: [(i - (i % 3)) / 3, i % 3],
+        },
+      ])
     );
-  }
+    setStepNum(h.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNum(step);
+    setXIsNext(step % 2 === 0);
+    setWinCell([]);
+    setWinner(null);
+    const h = history.slice(0, step + 1);
+    const current = h[h.length - 1];
+    const squares = current.squares.slice();
+    calculateWinner(squares, setWinner);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move
+      ? `Go to move (${step.stepLocate[0]},${step.stepLocate[1]}) `
+      : "Go to game start";
+    return (
+      <li key={move}>
+        <button
+          onClick={() => jumpTo(move)}
+          className={stepNum === move ? "back-btn bold-btn" : "back-btn"}
+        >
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={history[stepNum].squares}
+          winCell={winCell}
+          onClick={(i) => handleClick(i)}
+        />
+      </div>
+      <div className="game-info">
+        <div>
+          {stepNum === 9 && !winner
+            ? `Draw`
+            : winner
+            ? `Winner:${winner}`
+            : `Next player: ${xIsNext ? "X" : "O"}`}
+        </div>
+        <button
+          onClick={() => {
+            setReverse(!reverse);
+          }}
+        >
+          Reverse step
+        </button>
+        <ol reversed={reverse} className={reverse ? "reverse-list" : ""}>
+          {moves}
+        </ol>
+      </div>
+    </div>
+  );
 }
